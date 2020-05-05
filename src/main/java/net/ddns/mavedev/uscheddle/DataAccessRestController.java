54a90c8ef435b4da851e7dec2b8d53f0bfd9393b
mavedev <usatchoff@gmail.com>
2020-05-05 23:18:15 +0300

    The controller class renamed

--- src/main/java/net/ddns/mavedev/uscheddle/DataAccessRestController.java
+++ src/main/java/net/ddns/mavedev/uscheddle/DataAccessRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-public class TestController {
+public class DataAccessRestController {
     @GetMapping("/")
     public String test() {
         return "<h1>Hello</h1>";
