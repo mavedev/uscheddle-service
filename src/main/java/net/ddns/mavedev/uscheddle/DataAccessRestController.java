513572ebbfacd56a2afbdcc9b8da699a293abfaa
mavedev <usatchoff@gmail.com>
2020-05-06 00:12:26 +0300

    Post request for generating added

--- src/main/java/net/ddns/mavedev/uscheddle/DataAccessRestController.java
+++ src/main/java/net/ddns/mavedev/uscheddle/DataAccessRestController.java
@@ -3,8 +3,11 @@ package net.ddns.mavedev.uscheddle;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,6 +24,16 @@ public class DataAccessRestController {
                 put("testKey1", "testValue1");
                 put("testKey2", "testValue2");
                 put("testKey3", "testValue3");
+            }
+        };
+    }
+
+    @RequestMapping(value = "/generate", method = RequestMethod.POST)
+    public @ResponseBody Map<String, String> generate() {
+        return new HashMap<String, String>() {
+            private static final long serialVersionUID = 5276836687399797369L;
+            {
+                put("schedule_id", "10");
             }
         };
     }
