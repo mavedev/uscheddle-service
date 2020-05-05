3f37f63552a706adf9e8723d0036edd76a3c23b9
mavedev <usatchoff@gmail.com>
2020-05-05 23:42:53 +0300

    The schedule getting test request implemented

--- src/main/java/net/ddns/mavedev/uscheddle/DataAccessRestController.java
+++ src/main/java/net/ddns/mavedev/uscheddle/DataAccessRestController.java
@@ -1,14 +1,27 @@
 package net.ddns.mavedev.uscheddle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class DataAccessRestController {
-    @GetMapping("/")
-    public String test() {
-        return "<h1>Hello</h1>";
+    @RequestMapping(value = "/schedule/{id}", method = RequestMethod.GET)
+    public @ResponseBody Map<String, String> getSchedule(@PathVariable(value = "id") final String id) {
+        return new HashMap<String, String>() {
+            private static final long serialVersionUID = 5276836687399797368L;
+            {
+                put("testKey1", "testValue1");
+                put("testKey2", "testValue2");
+                put("testKey3", "testValue3");
+            }
+        };
     }
 }
