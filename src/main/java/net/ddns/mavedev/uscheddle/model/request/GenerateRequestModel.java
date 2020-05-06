8d853252ef39b3eca36745733693c168493a3d88
mavedev <usatchoff@gmail.com>
2020-05-06 18:46:28 +0300

    GenerateRequestModel complete

--- src/main/java/net/ddns/mavedev/uscheddle/model/request/GenerateRequestModel.java
+++ src/main/java/net/ddns/mavedev/uscheddle/model/request/GenerateRequestModel.java
@@ -1,5 +1,25 @@
 package net.ddns.mavedev.uscheddle.model.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GenerateRequestModel {
 
+    @JsonProperty("courses")
+    private CourseModel[] courses;
+
+    @JsonProperty("classes")
+    private ClassroomModel[] classrooms;
+
+    @JsonProperty("students")
+    private int students;
+
+    @JsonProperty("minInGroup")
+    private int minInGroup;
 }
