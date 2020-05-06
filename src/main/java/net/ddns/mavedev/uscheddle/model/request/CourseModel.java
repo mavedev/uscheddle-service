887ccf6ade386e3dd0cb6f9134ff6d76231f473b
mavedev <usatchoff@gmail.com>
2020-05-06 18:10:08 +0300

    Course model complete with lombok

--- src/main/java/net/ddns/mavedev/uscheddle/model/request/CourseModel.java
+++ src/main/java/net/ddns/mavedev/uscheddle/model/request/CourseModel.java
@@ -1,5 +1,34 @@
 package net.ddns.mavedev.uscheddle.model.request;
 
-public class CourseModel {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class CourseModel {
+    @Getter
+    @Setter
+    @Autowired
+    @JsonProperty("courseName")
+    private String name;
+
+    @Getter
+    @Setter
+    @Autowired
+    @JsonProperty("courseClassesType")
+    private String classesType;
+
+    @Getter
+    @Setter
+    @Autowired
+    @JsonProperty("courseInstaructor")
+    private String instructor;
+
+    @Getter
+    @Setter
+    @Autowired
+    @JsonProperty("courseHours")
+    private int hours;
 }
