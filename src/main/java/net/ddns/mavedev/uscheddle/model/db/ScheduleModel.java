5f4543178d8be7d8e6c8dd94fd3ea721b4b2b869
mavedev <usatchoff@gmail.com>
2020-05-06 21:22:12 +0300

    Test schedule DB model provided

--- src/main/java/net/ddns/mavedev/uscheddle/model/db/ScheduleModel.java
+++ src/main/java/net/ddns/mavedev/uscheddle/model/db/ScheduleModel.java
@@ -1,10 +1,17 @@
 package net.ddns.mavedev.uscheddle.model.db;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
+@Document(collection = "schedules")
 public class ScheduleModel {
+
+    @Id
+    private int id;
+    private String testField;
 
 }
