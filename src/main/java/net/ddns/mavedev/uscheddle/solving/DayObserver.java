a48860acb7df6f1f52b51971ae94d4400d071f16
mavedev <usatchoff@gmail.com>
2020-05-11 11:36:30 +0300

    Day observer provided

---
+++ src/main/java/net/ddns/mavedev/uscheddle/solving/DayObserver.java
@@ -0,0 +1,19 @@
+package net.ddns.mavedev.uscheddle.solving;
+
+import java.util.BitSet;
+import lombok.Data;
+
+@Data
+public class DayObserver {
+
+    private BitSet classesPerDayFlags;
+
+    private DayObserver(final int maxClassesPerDay) {
+        this.classesPerDayFlags = new BitSet(maxClassesPerDay);
+    }
+
+    public static DayObserver fromMaxClassesPerDay(final int maxClassesPerDay) {
+        return new DayObserver(maxClassesPerDay);
+    }
+
+}
