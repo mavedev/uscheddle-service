adbd75bef16fd83a93b239aa04bf764c73ac2d55
mavedev <usatchoff@gmail.com>
2020-05-11 11:46:34 +0300

    StudyLoad observer provided

---
+++ src/main/java/net/ddns/mavedev/uscheddle/solving/StudyLoadObserver.java
@@ -0,0 +1,17 @@
+package net.ddns.mavedev.uscheddle.solving;
+
+import lombok.Data;
+
+@Data
+public class StudyLoadObserver {
+
+    private static int DAYS_IN_WEEK = 6;
+    private DayObserver[] dayObservers = new DayObserver[DAYS_IN_WEEK];
+
+    public StudyLoadObserver(final int maxClassesPerDay) {
+        for (int i = 0; i < dayObservers.length; ++i) {
+            this.dayObservers[i] = DayObserver.fromMaxClassesPerDay(maxClassesPerDay);
+        }
+    }
+
+}
